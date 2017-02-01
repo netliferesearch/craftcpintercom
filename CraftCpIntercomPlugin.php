20289a88cfd1cf0c7b941caadda73cea8d77853a
kmelve <knut@netliferesearch.com>
2017-02-01 14:28:31 +0100

    only show for logged in users, send username and email

--- CraftCpIntercomPlugin.php
+++ CraftCpIntercomPlugin.php
@@ -38,14 +38,22 @@ class CraftCpIntercomPlugin extends BasePlugin
      *
      * @return mixed
      */
+     
     public function init()
     {
-      $intercomId = $this->settings['intercomId'];
-      $javascript = "window.intercomSettings = {
-          app_id: '{$intercomId}'
-        };
-        (function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/puws8gsr';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()";
-      craft()->templates->includeJs($javascript);
+      if (craft()->userSession->isLoggedIn()) {
+        $intercomId = $this->settings['intercomId'];
+        $name = craft()->userSession->name;
+        $email = craft()->userSession->getUser()->email;
+        $javascript = "
+          window.intercomSettings = {
+              app_id: '{$intercomId}',
+              name: '{$name}',
+              email: '{$email}',
+            };
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/puws8gsr';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()";
+          craft()->templates->includeJs($javascript);
+      }
     }
 
     /**
